bca6c01e04bc601a7426ed70539fbc0d51fa5263
Jeff Diers <jeff.diers@gmail.com>
2021-08-11 15:55:14 -0600

    removeing firebase config from gitignor for deployment.

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyA3wuNp0BR0iA8zSas853E80zaOnsn6JR8",
+  authDomain: "crwn-db-d6c63.firebaseapp.com",
+  projectId: "crwn-db-d6c63",
+  storageBucket: "crwn-db-d6c63.appspot.com",
+  messagingSenderId: "722142452074",
+  appId: "1:722142452074:web:c754aed293fbe90e41ed13",
+};
+
+export default config;
